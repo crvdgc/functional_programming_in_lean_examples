258e099c3db0175894e8e6896aed45904c092dde
crvdgc <ubikium@gmail.com>
2023-06-18 23:16:24 +0100

    C4.5 Standard Classes

--- C4.lean
+++ C4.lean
@@ -70,3 +70,38 @@ instance [HMul α α α]: HMul (PPoint α) α (PPoint α) where
 
 #eval {x := 2.5, y := 3.7 : PPoint Float} * 2.0
 
+structure NonEmptyList (α : Type) : Type where
+  head : α
+  tail : List α
+deriving Repr, BEq
+
+def NonEmptyList.ofList {α : Type}
+  | [] => none
+  | x :: xs => some ({ head := x, tail := xs } : NonEmptyList α)
+
+#eval NonEmptyList.ofList [1, 2, 3]
+
+def NonEmptyList.toList {α : Type} (ne : NonEmptyList α) := ne.head :: ne.tail
+
+instance : HAppend (List α) (NonEmptyList α) (NonEmptyList α) where
+  hAppend xs ne :=
+    match xs with
+    | [] => ne
+    | x :: xs => { head := x, tail := xs ++ NonEmptyList.toList ne }
+
+#eval [1,2,3] ++ NonEmptyList.mk 4 [5, 6]
+#eval ([] : List Nat) ++ NonEmptyList.mk 4 [5, 6]
+#eval [1,2,3] ++ NonEmptyList.mk 4 []
+
+inductive BinTree (α : Type) where
+  | leaf : BinTree α
+  | branch : BinTree α → α → BinTree α → BinTree α
+deriving Repr
+
+def BinTree.map (f : α → β)
+    | BinTree.leaf => BinTree.leaf
+    | BinTree.branch left x right =>
+      BinTree.branch (BinTree.map f left) (f x) (BinTree.map f right)
+
+instance : Functor BinTree where
+  map := BinTree.map

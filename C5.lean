576ed0020d8eaa1a5fa42c9a5a206186391f3214
crvdgc <ubikium@gmail.com>
2023-06-27 09:44:23 +0100

    c5

--- C5.lean
+++ C5.lean
@@ -39,6 +39,18 @@ def evaluateM [Monad m] (applySpecial : special → Int → Int → m Int): Expr
     evaluateM applySpecial e2 >>= fun v2 =>
     applyPrim applySpecial p v1 v2
 
+def evaluateM' [Monad m] (applySpecial : special → Int → Int → m Int) : Expr (Prim special) → m Int
+  | Expr.const i => pure i
+  | Expr.prim p e1 e2 => do
+    let v1 ← evaluateM applySpecial e1
+    let v2 ← evaluateM applySpecial e2
+    applyPrim applySpecial p v1 v2
+
+def evaluateM'' [Monad m] (applySpecial : special → Int → Int → m Int) : Expr (Prim special) → m Int
+  | Expr.const i => pure i
+  | Expr.prim p e1 e2 => do
+    applyPrim applySpecial p (← evaluateM applySpecial e1) (← evaluateM applySpecial e2)
+
 def applyEmpty [Monad m] (op : Empty) (_ : Int) (_ : Int) : m Int :=
   nomatch op
 
@@ -128,7 +140,7 @@ theorem many_union_more : Many.union (Many.more x xs) ys = Many.more x (fun () =
 #eval (Many.bind (Many.union (Many.one 1) (Many.fromList [1, 2, 3])) Many.one).toList
 #eval let f := Many.one; (Many.union (Many.bind (Many.one 1) f) (Many.bind (Many.fromList [1, 2, 3]) f)).toList
 
-theorem many_union_assoc_right : Many.union (Many.union xs ys) zs = Many.union xs (Many.union ys zs) := by
+theorem many_union_assoc : Many.union (Many.union xs ys) zs = Many.union xs (Many.union ys zs) := by
   induction xs with
   | none => rfl
   | more x xs ih =>
@@ -143,7 +155,7 @@ theorem many_bind_union_distr : Many.bind (Many.union xs ys) f = Many.union (Man
     rw [many_union_more]
     rw [many_bind_more]
     rw [ih]
-    rw [many_union_assoc_right]
+    rw [many_union_assoc]
 
 theorem many_monad_assoc : Many.bind (Many.bind v f) g = Many.bind v (fun x => Many.bind (f x) g) := by
   induction v with
@@ -153,4 +165,53 @@ theorem many_monad_assoc : Many.bind (Many.bind v f) g = Many.bind v (fun x => M
     repeat rw [many_bind_more]
     rw [many_bind_union_distr]
     rw [ih]
+
+def Reader (ρ : Type) (α : Type) : Type := ρ → α
+
+def read : Reader ρ ρ := fun env => env
+
+def Reader.pure (x : α) : Reader ρ α := fun _ => x
+
+def Reader.bind {ρ : Type} {α : Type} {β : Type}
+  (result : ρ → α) (next : α → ρ → β) : ρ → β :=
+  fun env => next (result env) env
+
+theorem reader_monad_identity_left : Reader.bind (Reader.pure v) f = f v := by rfl
+
+theorem reader_monad_identity_right : Reader.bind mv Reader.pure = mv := by rfl
+
+theorem reader_monad_assoc :
+  Reader.bind (Reader.bind mv f) g =
+  Reader.bind mv (fun v => Reader.bind (f v) g) := by rfl
+
+structure WithLog (logged : Type) (α : Type) where
+  log : List logged
+  val : α
+
+instance : Monad (WithLog logged) where
+  pure x := {log := [], val := x}
+  bind result next :=
+    let {log := thisOut, val := thisRes} := result
+    let {log := nextOut, val := nextRes} := next thisRes
+    {log := thisOut ++ nextOut, val := nextRes}
+
+inductive ToTrace (α : Type) : Type where
+  | trace : α → ToTrace α
+
+
+def applyTraced : ToTrace (Prim Empty) → Int → Int → WithLog (Prim Empty × Int × Int) Int :=
+  λ (ToTrace.trace prim) x y =>
+  let evalEmpty : Prim Empty → Int → Int → Int
+    | Prim.plus, x, y => x + y
+    | Prim.minus, x, y => x - y
+    | Prim.times, x, y => x * y
+  { log := [(prim, x, y)], val :=  evalEmpty prim x y }
+
+
+deriving instance Repr for WithLog
+deriving instance Repr for Empty
+deriving instance Repr for Prim
+
+open Expr Prim ToTrace in
+#eval evaluateM applyTraced (prim (other (trace times)) (prim (other (trace plus)) (const 1) (const 2)) (prim (other (trace minus)) (const 3) (const 4)))
 

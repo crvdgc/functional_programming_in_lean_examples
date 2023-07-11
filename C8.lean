16710c932001fe35fbf7035f55916285b9fcffde
crvdgc <ubikium@gmail.com>
2023-07-11 19:38:44 +0100

    chapter 8.2 universe design pattern

--- C8.lean
+++ C8.lean
@@ -56,3 +56,118 @@ def Vect.take : (n : Nat) → Vect α (k + n) → Vect α n
   | n + 1, .cons x xs => .cons x (take n xs)
 
 #eval danishPeaks.take 2
+
+inductive Finite where
+  | empty : Finite
+  | unit : Finite
+  | bool : Finite
+  | pair : Finite → Finite → Finite
+  | arr : Finite → Finite → Finite
+  | option : Finite → Finite
+
+abbrev Finite.asType : Finite → Type
+  | .empty => Empty
+  | .unit => Unit
+  | .bool => Bool
+  | .pair t1 t2 => asType t1 × asType t2
+  | .arr t1 t2 => asType t1 → asType t2
+  | .option t => Option t.asType
+
+def List.product (xs : List α) (ys : List β) : List (α × β) := Id.run do
+  let mut out : List (α × β) := []
+  for x in xs do
+    for y in ys do
+      out := (x, y) :: out
+  pure out.reverse
+
+mutual
+  def Finite.functions (t : Finite) (results : List α) : List (t.asType → α) :=
+    match t with
+      | .empty => []
+      | .unit =>
+        results.map fun r =>
+          fun () => r
+      | .bool =>
+        (results.product results).map fun (r1, r2) =>
+          fun
+            | true => r1
+            | false => r2
+      | .pair t1 t2 =>
+        let f1s := t1.functions <| t2.functions results
+        f1s.map fun f =>
+          fun (x, y) =>
+            f x y
+      | .arr t1 t2 =>
+        let args := t1.enumerate
+        let base :=
+          results.map fun r =>
+            fun _ => r
+        args.foldr
+          (fun arg rest =>
+            (t2.functions rest).map fun more =>
+              fun f => more (f arg) f)
+          base
+      | .option t =>
+        let fs := t.functions results
+        (results.product fs).map fun (r, f) =>
+          fun
+            | .none => r
+            | .some v => f v
+  
+  def Finite.enumerate (t : Finite) : List t.asType :=
+    match t with
+    | .empty => []
+    | .unit => [()]
+    | .bool => [true, false]
+    | .pair t1 t2 => t1.enumerate.product t2.enumerate
+    | .arr t1 t2 => t1.functions t2.enumerate
+    | .option t => .none :: t.enumerate.map .some
+end
+
+def Finite.beq (t : Finite) (x y : t.asType) : Bool :=
+  match t with
+  | .empty => true
+  | .unit => true
+  | .bool => x == y
+  | .pair t1 t2 => beq t1 x.fst y.fst && beq t2 x.snd y.snd
+  | .arr t1 t2 =>
+    t1.enumerate.all fun arg => beq t2 (x arg) (y arg)
+  | .option t =>
+    match x, y with
+    | .none, .none => true
+    | .some x, .some y => beq t x y
+    | _, _ => false
+
+def Finite.to_string (t : Finite) (x : t.asType) : String :=
+  match t with
+  | .empty => "impossible"
+  | .unit => "()"
+  | .bool => if x then "true" else "false"
+  | .pair t1 t2 =>
+    let (x1, x2) := x
+    let s1 := t1.to_string x1
+    let s2 := t2.to_string x2
+    s!"({s1}, {s2})"
+  | .arr t1 t2 =>
+    let rows :=
+      let args := t1.enumerate
+      let values := args.map x
+      List.zipWith (s!"{·} ↦ {·}") (args.map t1.to_string) (values.map t2.to_string)
+    String.intercalate "\n" rows
+  | .option t =>
+    match x with
+    | .none => "none"
+    | .some v => s!"some {t.to_string v}"
+
+#eval Finite.to_string .unit ()
+#eval IO.println (Finite.to_string (.arr .bool .bool) not)
+
+def Finite.print_all (code : Finite) : IO Unit :=
+  for f in Finite.enumerate code do
+    IO.println "---"
+    IO.println (Finite.to_string code f)
+
+#eval Finite.print_all (.arr .bool .bool)
+
+#eval Finite.print_all (.arr (.option .bool) .bool)
+
